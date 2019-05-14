c450db1cb38c3958dc7358a46ed8c2d8e8d3cfcc
yahiaouik <kenza.yahiaoui@telecom-st-etienne.fr>
2019-05-14 16:22:17 +0200

    classe post + test main

---
+++ src/main/java/tse/hppproject/Post.java
@@ -0,0 +1,67 @@
+package tse.hppproject;
+
+import java.sql.Timestamp;
+
+public class Post {
+	
+	Timestamp ts;
+	Integer post_id;
+	Integer user_id;
+	String user;
+	
+	
+	public Post(String str) {
+		super();
+		String[] string = str.replace("|", ",").split(",");
+		this.ts=Timestamp.valueOf(string[0].substring(0,string[0].indexOf(".")).replaceAll("T", " "));
+		this.post_id=Integer.parseInt(string[1]);
+		this.user_id=Integer.parseInt(string[2]);
+		this.user=string[4];
+		
+	}
+
+
+	public Timestamp getTs() {
+		return ts;
+	}
+
+
+	public void setTs(Timestamp ts) {
+		this.ts = ts;
+	}
+
+
+	public Integer getPost_id() {
+		return post_id;
+	}
+
+
+	public void setPost_id(Integer post_id) {
+		this.post_id = post_id;
+	}
+
+
+	public Integer getUser_id() {
+		return user_id;
+	}
+
+
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+
+
+	public String getUser() {
+		return user;
+	}
+
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+		
+	
+
+}
+
+
--- src/main/java/tse/hppproject/my_project.java
+++ src/main/java/tse/hppproject/my_project.java
@@ -1,6 +1,15 @@
 package tse.hppproject;
 
 public class my_project {
+	
+	public static void main(String[] args) {
+		
+		Post pst = new Post("2010-02-01T05:12:32.921+0000|1039993|3981||Lei Liu");
+		System.out.println(pst.getTs());
+		System.out.println(pst.getUser());
+		System.out.println(pst.getUser_id());
+		System.out.println(pst.getPost_id());
+	}
 
-	//wesh les gars
+
 }
