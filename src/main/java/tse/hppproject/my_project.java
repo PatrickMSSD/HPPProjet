0d644eda3537b12b5b40c8f485425ea792a6883a
yahiaouik <kenza.yahiaoui@telecom-st-etienne.fr>
2019-05-14 18:03:06 +0200

    Time test

--- src/main/java/tse/hppproject/my_project.java
+++ src/main/java/tse/hppproject/my_project.java
@@ -1,6 +1,7 @@
 package tse.hppproject;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.function.Consumer;
@@ -11,17 +12,42 @@ public class my_project {
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
+		Timestamp total_time = new Timestamp(0);
+		
 		BlockingQueue<String> post_queue = new ArrayBlockingQueue<String>(110);
 		BlockingQueue<String> comm_queue = new ArrayBlockingQueue<String>(110);
+		BlockingQueue<Object> total_queue = new ArrayBlockingQueue<Object>(110);
 		
-		//Producer prod_post = new Producer(post_queue,"C:\\Users\\kenza\\git\\HPPProjet\\resourses\\posts.dat");
+		Producer prod_post = new Producer(post_queue,"C:\\Users\\kenza\\git\\HPPProjet\\resourses\\posts.dat");
 		Producer prod_comm = new Producer(comm_queue,"C:\\Users\\kenza\\git\\HPPProjet\\resourses\\comments.dat");
 		
-		//prod_post.readFile(1);
-		prod_comm.readFile(2);
+		
+		prod_post.readFile(50);
+		prod_comm.readFile(50);
 		
 		Comment com1 = new Comment(comm_queue.poll());
-		System.out.println(com1.toString());
+		Post post1 = new Post(post_queue.poll());
+		
+		while(!comm_queue.isEmpty()&& !post_queue.isEmpty()) {
+			
+			if(post1.getTs().before(com1.getTs())) {
+				total_queue.add(post1);
+				total_time=post1.getTs();
+				System.out.println("c'est un post");
+				post1 = new Post(post_queue.poll());
+			}else {
+				total_queue.add(com1);
+				total_time=com1.getTs();
+				com1 = new Comment(comm_queue.poll());
+				System.out.println("c'est un comm");
+			}
+		}
+		
+		System.out.println(total_queue.toString());
+		System.out.println(total_time.toString());
+		
+		
+		
 		
 	}
 
