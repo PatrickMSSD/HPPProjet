b925ed65ffa4e7f495cf687bda0c8feb07fe1723
MP <MP@MPC>
2019-05-15 15:18:10 +0200

    " Creation of the HashMap of Post which will be sorted by score later "

--- src/main/java/tse/hppproject/my_project.java
+++ src/main/java/tse/hppproject/my_project.java
@@ -2,6 +2,9 @@ package tse.hppproject;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.function.Consumer;
@@ -42,12 +45,37 @@ public class my_project {
 				System.out.println("c'est un comm");
 			}
 		}
+
 		
-		System.out.println(total_queue.toString());
-		System.out.println(total_time.toString());
+		// HASH MAP 
 		
+		Map<Integer, ArrayList<Comment>> IDPost2Com = new HashMap<Integer, ArrayList<Comment>>();
+		Map<Integer, Post> ID2Post = new HashMap<Integer, Post>();
+		Map<Integer, Integer> IDCom2IDPost = new HashMap<Integer, Integer>();
+		while(!total_queue.isEmpty()) {
+			if (total_queue.peek().getClass().equals(Post.class) ) {
+				Post patchPost = (Post) total_queue.take();
+				IDPost2Com.put(patchPost.post_id, new ArrayList<Comment>());
+				ID2Post.put(patchPost.post_id, patchPost);
+			}
+			else {
+				Comment patchComment = (Comment) total_queue.take();
+				if (patchComment.getId_post() >0) {
+					IDPost2Com.get(patchComment.getId_post()).add(patchComment);
+					IDCom2IDPost.put(patchComment.id_comment, patchComment.id_post);
+				}
+				else {
+					
+					IDPost2Com.get(IDCom2IDPost.get(patchComment.id_replied)).add(patchComment);
+					IDCom2IDPost.put(patchComment.id_comment, IDCom2IDPost.get(patchComment.id_replied));
+					
+				}
+			}
+		}
 		
-		
+		System.out.println(IDPost2Com.toString());
+		System.out.println(ID2Post.toString());
+		System.out.println(IDCom2IDPost.toString());
 		
 	}
 
